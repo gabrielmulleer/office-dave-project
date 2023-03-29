591110acc49466fe3f53a7da79360d8b3f7b7bf8
gabrielmulleer <emaildegabrielm@gmail.com>
2023-03-29 12:54:21 -0300

    feature: add custom-radio3.css for step5.html

--- assets/js/star-rating.js
+++ assets/js/star-rating.js
@@ -1,13 +1,15 @@
-const stars = document.querySelectorAll('.star-icon');
+const stars = document.querySelectorAll(".star-icon");
+stars.forEach((element) => {
+  element.addEventListener("click", evento);
+});
 
-document.addEventListener('click', evento)
 function evento(e) {
-    const classStar = e.target.classList;
-    if (!classStar.contains('ativo')) {
-        stars.forEach(element => {
-            element.classList.remove('ativo');
-        });
-        classStar.add('ativo');
-        console.log(e.target.getAttribute('data-rating'))
-    }
-};
+  const classStar = e.target.classList;
+  if (!classStar.contains("ativo")) {
+    stars.forEach((element) => {
+      element.classList.remove("ativo");
+    });
+    classStar.add("ativo");
+    console.log(e.target.getAttribute("data-rating"));
+  }
+}
